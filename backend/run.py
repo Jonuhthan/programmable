2f909e816bf5d6aaa5d8e32932adb12bb488f7e2
kyle-luong <133829822+kyle-luong@users.noreply.github.com>
2024-10-12 19:28:16 -0400

    Created entry point to run Flask app

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,7 @@
 # Entry point to run Flask app
 from src import create_app
 
-app = create_app()  # Create the Flask app
+app = create_app()  # Create Flask app
 
 if __name__ == "__main__":
-    app.run(debug=True)  # Run the app in debug mode
+    app.run(debug=True)  # Run app in debug mode
