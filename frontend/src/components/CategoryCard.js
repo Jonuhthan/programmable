4d786adf86ddce6d8586b80031882863232eef0c
Jonathan Lam <jonhaolam@gmail.com>
2024-10-12 22:31:26 -0400

    Added endpoints and links to topics

--- frontend/src/components/CategoryCard.js
+++ frontend/src/components/CategoryCard.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './CategoryCard.css';
 
 // represents each individual box where users can choose which questions to be tested on
-const CategoryCard = ({ title, description, imageSrc }) => {
+const CategoryCard = ({ title, description, imageSrc, uri }) => {
   return (
-    <div className="category-card">
-      <img src={imageSrc} alt={title} className="category-image" />
-      <div className="category-info">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
+    <a className='uri' href={uri}>
+      <div className="category-card">
+        <img src={imageSrc} alt={title} className="category-image" />
+        <div className="category-info">
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
     </div>
+    </a>
   );
 };
 
