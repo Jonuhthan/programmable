0b4d419212e4fb25233c2bf0689c5d9ca29dbc69
Jonathan Lam <jonhaolam@gmail.com>
2024-10-12 22:18:01 -0400

    Fixed spacing and added 3 more topics

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -5,6 +5,9 @@ import StreakBar from '../components/StreakBar';
 import javaImage from '../assets/images/java.png';
 import pythonImage from '../assets/images/python.png';
 import sqlImage from '../assets/images/mySQL.png';
+import dsaImage from '../assets/images/dsa.png';
+import jsImage from '../assets/images/javascript.png';
+import cppImage from '../assets/images/cpp.png';
 import './HomePage.css';
 
 const HomePage = () => {
@@ -12,6 +15,9 @@ const HomePage = () => {
         { title: 'Java Foundations', description: 'Practice the foundational skills of Java', imageSrc: javaImage },
         { title: 'Python', description: 'Practice Python basics', imageSrc: pythonImage },
         { title: 'mySQL', description: 'Pratice mySQL basics', imageSrc: sqlImage },
+        { title: 'Data Structures & Algorithms', description: 'Master your DSA skills', imageSrc: dsaImage },
+        { title: 'JavaScript', description: 'Sharpen your JavaScript skills', imageSrc: jsImage },
+        { title: 'C++', description: 'Improve your C++ programming skills', imageSrc: cppImage }
       ];
 
     return (
